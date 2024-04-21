a83b9e2740fc87db0b5c35cc8c477186a0741bb0
Rhito <lasnguyen06@gmail.com>
2024-04-21 09:21:27 +0700

    lab01--lab1_2

---
+++ MVC ASP.NET_LEARNING/lab01-lesson01/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using lab01_lesson01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab01_lesson01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ActionResult Index()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+}
---
+++ MVC ASP.NET_LEARNING/lab_1.2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using lab_1._2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab_1._2.Controllers
+{
+    public class HomeController : Controller { 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "Research IT - DEVMASTER / Chúng tôi sẽ làm bạn hài lòng!";
+            return View();
+        }
+        //GET: Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thông tin liên hệ";
+            return View();
+        }
+        //GET: Home/Products
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Thực đơn hôm nay";
+            string[] products = {"Gà quay","Cá chép om dưa","Vịt quay vân đình","Ốc xào xả ớt"};
+            ViewBag.products = products;
+            return View();
+        }
+    } }
