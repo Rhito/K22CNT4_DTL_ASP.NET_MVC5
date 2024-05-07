83ee5a08b1b6312069c7708cf0c1cf9aae05eafa
Rhito <lasnguyen06@gmail.com>
2024-05-07 21:56:10 +0700

    dtl_lesson02_preLab

---
+++ MVC ASP.NET/Lesson01/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Lesson01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            ViewBag.name = "Dinh Tien Luc";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+        public ActionResult ViewObj()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC ASP.NET/Lesson01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
